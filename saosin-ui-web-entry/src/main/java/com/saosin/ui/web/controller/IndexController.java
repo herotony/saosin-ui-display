5fd4f302f2dd580d87a671df8acc7c65d7121b36
herotony <herotony@gmail.com>
2018-08-27 18:19:00 +0800

    init debug mode ok

--- saosin-ui-web-entry/src/main/java/com/saosin/ui/web/controller/IndexController.java
+++ saosin-ui-web-entry/src/main/java/com/saosin/ui/web/controller/IndexController.java
@@ -1,7 +1,21 @@
 package com.saosin.ui.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by saosin on 2018/8/27.
  */
+
+@Controller
+@RequestMapping("/commonui")
 public class IndexController {
+
+    @RequestMapping(value = "/simple.do",method = RequestMethod.POST)
+    @ResponseBody
+    public String init(){
+        return "world";
+    }
 }
